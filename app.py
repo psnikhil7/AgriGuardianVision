204e750599d4e14bf34d904640f47dff0252490d
psnikhil7 <psnikhil@karunya.edu.in>
2026-03-14 13:50:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,26 @@ import zipfile
 MODEL_PATH = "leaf_disease_model.keras"
 MODEL_ZIP = "leaf_model.zip"
 
+
 def ensure_model_exists():
     if os.path.exists(MODEL_PATH):
-        return
+        return MODEL_PATH
 
     if os.path.exists(MODEL_ZIP):
         with zipfile.ZipFile(MODEL_ZIP, "r") as zip_ref:
             zip_ref.extractall(".")
 
-ensure_model_exists()
+    # search everywhere after extraction
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file == "leaf_disease_model.keras":
+                return os.path.join(root, file)
 
-model = tf.keras.models.load_model(MODEL_PATH)
+    return MODEL_PATH
+
+
+REAL_MODEL_PATH = ensure_model_exists()
+model = tf.keras.models.load_model(REAL_MODEL_PATH)
 
 with open("class_names.txt") as f:
     class_names = [c.strip() for c in f.readlines()]
@@ -28,19 +37,21 @@ st.set_page_config(page_title="AgriGuardian Vision", layout="centered")
 st.title("🌿 AgriGuardian Vision")
 st.write("Upload a leaf image to detect plant disease.")
 
-uploaded_file = st.file_uploader("Upload leaf image", type=["jpg","png","jpeg"])
+uploaded_file = st.file_uploader("Upload leaf image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file:
+    image = Image.open(uploaded_file).convert("RGB")
+    display_image = image.copy()
 
-    image = Image.open(uploaded_file).resize((224,224))
-    img_array = np.array(image)/255.0
+    image = image.resize((224, 224))
+    img_array = np.array(image) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
     prediction = model.predict(img_array)
     index = np.argmax(prediction)
     confidence = prediction[0][index]
 
-    st.image(image, caption="Uploaded Leaf", width=300)
+    st.image(display_image, caption="Uploaded Leaf", width=300)
 
     st.success(f"Prediction: {class_names[index]}")
     st.write(f"Confidence: {confidence*100:.2f}%")
