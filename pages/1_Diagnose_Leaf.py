8ece4d3ddb9eea9d96492b28e6d9adc9c958a158
psnikhil7 <psnikhil@karunya.edu.in>
2026-03-15 20:17:49 +0530

    Create 1_Diagnose_Leaf.py

---
+++ pages/1_Diagnose_Leaf.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Diagnose Leaf", page_icon="🌿", layout="wide")
+
+st.title("🌿 Diagnose Leaf")
+
+st.write("Upload a leaf image here to check if it is healthy or diseased.")
